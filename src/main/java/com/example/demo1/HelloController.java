eb0bb4055578301b9c1e2f87e43a55bc2d22ab07
mykytkas <main.capybara@gmail.com>
2024-02-06 14:40:27 +0100

    added listener and scale to slider

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -1,8 +1,11 @@
 package com.example.demo1;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -12,8 +15,10 @@ import javafx.scene.control.Slider;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HelloController {
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
 
@@ -23,12 +28,27 @@ public class HelloController {
     @FXML
     private Button button;
 
+    private int number;
     private Parent root;
     private Stage stage;
     private Scene scene;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        slider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                number = (int) slider.getValue();
+                welcomeText.setText("Selected: " + number + " nodes");
+            }
+        });
+    }
+
+
     @FXML
     protected void onHelloButtonClick(ActionEvent event) throws IOException {
-        int number = (int)slider.getValue();
+        number = (int)slider.getValue();
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("automaton-view.fxml"));
         root = loader.load();
