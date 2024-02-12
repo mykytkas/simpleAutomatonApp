61eea5f61b25a458c14060d4e32294422e01274a
mykytkas <main.capybara@gmail.com>
2024-02-12 17:53:13 +0100

    copied FiniteAutomata from my other project

---
+++ src/main/java/com/example/demo1/FiniteAutomata.java
@@ -0,0 +1,8 @@
+package com.example.demo1;
+
+public interface FiniteAutomata {
+    public void addVertex(boolean start, boolean end, String name);
+    public Vertex getVertexById(int id);
+    public void deleteVertex(int id);
+    public boolean isAcceptableString(String string);
+}
